ce5e13120cb3f09381be197ad5cd4b38298a3730
jyw2671 <yjung@student.42seoul.kr>
2022-02-09 17:33:18 +0900

    add setup.sh & 50-server.cnf

---
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+service mysql start
+mysql -e "CREATE DATABASE IF NOT EXISTS ${WP_DB_NAME}; \
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; \
+GRANT ALL ON ${WP_DB_NAME}.* TO '${MYSQL_USER}'@'%'; \
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; \
+FLUSH PRIVILEGES;"
