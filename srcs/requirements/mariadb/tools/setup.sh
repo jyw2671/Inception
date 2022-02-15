47b63d929311ba764fc5ef98b1dfc800fab661d4
jyw2671 <yjung@student.42seoul.kr>
2022-02-15 17:45:26 +0900

    finish mariadb setup

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# create DATABASE
 service mysql start
 mysql -e "CREATE DATABASE IF NOT EXISTS ${WP_DB_NAME}; \
 CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; \
 GRANT ALL ON ${WP_DB_NAME}.* TO '${MYSQL_USER}'@'%'; \
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; \
 FLUSH PRIVILEGES;"
+
+# exec foreground
+service mysql stop
+exec mysqld_safe
