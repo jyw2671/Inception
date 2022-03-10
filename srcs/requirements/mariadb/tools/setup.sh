85dc1613ebbf3f4233839ea06b595316b7cb1958
jyw2671 <yjung@student.42seoul.kr>
2022-03-10 20:46:33 +0900

    fix code

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# create DATABASE
-service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS ${WP_DB_NAME}; \
-CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; \
-GRANT ALL ON ${WP_DB_NAME}.* TO '${MYSQL_USER}'@'%'; \
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; \
-FLUSH PRIVILEGES;"
+if [ ! -d /var/lib/mysql/$WP_DB_NAME ]; then
+    # mysql setting
+    service mysql start
+    mysql -e "CREATE DATABASE IF NOT EXISTS ${WP_DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci; \
+    CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; \
+    GRANT ALL ON ${WP_DB_NAME}.* TO '${MYSQL_USER}'@'%'; \
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; \
+    FLUSH PRIVILEGES;"
+    mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} shutdown
+fi
 
+# service mysql stop
 # exec foreground
-service mysql stop
 exec mysqld_safe
