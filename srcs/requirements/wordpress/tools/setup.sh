d4fbd2e2eb1def806981783c8f56c53329016860
jyw2671 <yjung@student.42seoul.kr>
2022-03-10 20:47:07 +0900

    fix code

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -e "/var/www/html/index.php" ]; then
+if [ ! -e /var/www/html/index.php ]; then
 	# wordpress setup
 	cp -R /wordpress/* /var/www/html/
 	rm /var/www/html/wp-config-sample.php
@@ -9,12 +9,12 @@ if [ ! -e "/var/www/html/index.php" ]; then
 
 	# wordpress cli(command line interface) install
 	wp core download --locale=ko_KR --allow-root
-	until wp core install --url=$DOMAIN_NAME --title='WP for inception' --admin_user=yjung --admin_password=password --admin_email=yjung@student.42seoul.com --skip-email --path='/var/www/html/'; do
+	until wp core install --url=${DOMAIN_NAME} --title='WP for inception' --admin_user=yjung --admin_password=password --admin_email=yjung@student.42seoul.com --skip-email --path='/var/www/html/'; do
 	sleep 0.5
 	done
 
 	# make wordpress user
-	wp user create --allow-root $WP_USER_ID $WP_USER_EMAIL --user_pass=$WP_USER_PW --role=author
+	wp user create --allow-root ${WP_USER_ID} ${WP_USER_EMAIL} --user_pass=${WP_USER_PW} --role=author
 fi
 
 # run wordpress foreground
