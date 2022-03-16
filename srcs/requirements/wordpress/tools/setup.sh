8708cba0bfd8a283fec401721bef65d6e13857ce
jyw2671 <yjung@student.42seoul.kr>
2022-03-16 19:35:41 +0900

    finish wordpress set

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -13,10 +13,9 @@ if ! wp core is-installed --allow-root --path='/var/www/html'; then
 	chown -R www-data:www-data /var/www/html
 
 	# wordpress cli(command line interface) install
-	wp core download --locale=ko_KR --allow-root
+	wp core download --locale=ko_KR --allow-root --path='/var/www/html'
 	wp core install --url=$DOMAIN_NAME --title='WP for inception' --admin_user=$MYSQL_USER\
-	 --admin_password=$MYSQL_PASSWORD --admin_email=$MYSQL_EMAIL --allow-root\
-	 --skip-email --path='/var/www/html/'
+		--admin_password=$MYSQL_PASSWORD --admin_email=$MYSQL_EMAIL --allow-root --path='/var/www/html/'
 
 	# make wordpress user
 	wp user create --allow-root $WP_USER_ID $WP_USER_EMAIL --user_pass=$WP_USER_PW --role=author --path='/var/www/html'
