8f69ef184fb26e676ef72c0dbe77b3d66e0aac7f
jyw2671 <yjung@student.42seoul.kr>
2022-03-16 18:42:08 +0900

    fix until position

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-if [ ! -e /var/www/html/index.php ]; then
+until mysql --host=mariadb --user=$MYSQL_USER --password=$MYSQL_PASSWORD -e '\c'; do
+  echo >&2 "mariadb is unavailable - sleeping"
+  sleep 1
+done
+
+if ! wp core is-installed --allow-root --path='/var/www/html'; then
 	# wordpress setup
 	cp -R /wordpress/* /var/www/html/
 	rm /var/www/html/wp-config-sample.php
@@ -9,13 +14,14 @@ if [ ! -e /var/www/html/index.php ]; then
 
 	# wordpress cli(command line interface) install
 	wp core download --locale=ko_KR --allow-root
-	until wp core install --url=${DOMAIN_NAME} --title='WP for inception' --admin_user=yjung --admin_password=password --admin_email=yjung@student.42seoul.com --allow-root --skip-email --path='/var/www/html/'; do
-	sleep 0.5
-	done
+	wp core install --url=$DOMAIN_NAME --title='WP for inception' --admin_user=$MYSQL_USER\
+	 --admin_password=$MYSQL_PASSWORD --admin_email=$MYSQL_EMAIL --allow-root\
+	 --skip-email --path='/var/www/html/'
 
 	# make wordpress user
-	wp user create --allow-root ${WP_USER_ID} ${WP_USER_EMAIL} --user_pass=${WP_USER_PW} --role=author
+	wp user create --allow-root $WP_USER_ID $WP_USER_EMAIL --user_pass=$WP_USER_PW --role=author --path='/var/www/html'
 fi
 
 # run wordpress foreground
+service php7.3-fpm stop
 exec php-fpm7.3 --nodaemonize
